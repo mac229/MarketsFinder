d1bca28ff497d888cad37d6903db7827537d7597
mac229 <mac229@interia.eu>
2015-06-01 00:26:31 +0200

    "Place" class

---
+++ app/src/main/java/com/maciejkozlowski/marketsfinder/Data/Place.java
@@ -0,0 +1,13 @@
+package com.maciejkozlowski.marketsfinder.Data;
+
+public class Place {
+
+    public String street;
+    public String number;
+    public String postal_code;
+    public String city;
+
+    public double lng;
+    public double lat;
+
+}
