0738567a72ae71485ff066f6f075608057f0b0c1
mac229 <mac229@interia.eu>
2015-06-03 01:10:34 +0200

    Saving and reading previous lat and lon

--- app/src/main/java/com/maciejkozlowski/marketsfinder/Helpers/MySharedPreferences.java
+++ app/src/main/java/com/maciejkozlowski/marketsfinder/Helpers/MySharedPreferences.java
@@ -14,6 +14,8 @@ public class MySharedPreferences {
     public static final String MY_PREFS = "MY_PREFS";
     private static final String first_run = "first_run";
     private static final String province = "province";
+    private static final String lat = "lat";
+    private static final String lon = "lon";
 
     public static boolean isFirstRun(Context context){
         SharedPreferences prefs = context.getSharedPreferences(MY_PREFS, Context.MODE_PRIVATE);
@@ -32,9 +34,13 @@ public class MySharedPreferences {
 
         if (MyLocation.province == null) {
             MyLocation.province = prefs.getString(province, "");
+            MyLocation.lat = prefs.getFloat(lat, 0);
+            MyLocation.lon = prefs.getFloat(lon, 0);
         } else {
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString(province, MyLocation.province);
+            editor.putFloat(lat, (float) MyLocation.lat);
+            editor.putFloat(lon, (float) MyLocation.lon);
             editor.apply();
         }
         Log.i("#hashtag", String.valueOf(MyLocation.lat));
--- app/src/main/java/com/maciejkozlowski/marketsfinder/MapsActivity.java
+++ app/src/main/java/com/maciejkozlowski/marketsfinder/MapsActivity.java
@@ -54,6 +54,7 @@ public class MapsActivity extends FragmentActivity {
             map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
             map.setMyLocationEnabled(true);
             map.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52, 19), 5.5f));
+
         }
     }
 
